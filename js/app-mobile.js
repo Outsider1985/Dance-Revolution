56c8323cc375db0be03979926cff3667797398c4
Outsider1985 <javieru2001@gmail.com>
2022-04-01 18:27:11 +0200

    push 01/04

--- js/app-mobile.js
+++ js/app-mobile.js
@@ -2,8 +2,10 @@ import {renderMenu} from './mobile/menu.js';
 import {plusMinusBtns} from './desktop/plusMinusBtns.js';
 import {tabBtns} from './desktop/tabBtns.js';
 import {form} from './desktop/form.js';
+import {ckeditor} from '@ckeditor/ckeditor5-build-classic';
 
 renderMenu();
 plusMinusBtns();
 tabBtns();
 form();
+ckeditor();
